94d5c334557cb98138f1d709efb4775101fb8d07
Evan Brumley <evan.brumley@gmail.com>
2014-08-04 00:41:33 +1000

    Extra stuff for workshop

--- server.py
+++ server.py
@@ -1,5 +1,24 @@
+"""PS Move Restful Server
+
+Usage:
+  server.py [--battery-saver]
+
+Options:
+  --battery_saver  If set, only allows a few seconds without a GET request before shutting off a controllers LEDs and rumble
+
+"""
+from docopt import docopt
+import datetime
+import time
+from threading import Thread
 from flask import Flask
 from flask.ext.restful import reqparse, abort, Api, Resource
+
+try:
+    from settings import CONTROLLER_SERIALS
+except ImportError:
+    CONTROLLER_SERIALS = {}
+    
 
 from utils import get_controllers
 
@@ -7,6 +26,10 @@ from utils import get_controllers
 
 app = Flask(__name__)
 api = Api(app)
+
+
+
+controllers = get_controllers()
 
 
 def color_type(val):
@@ -28,17 +51,29 @@ def rumble_type(val):
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('color_red', type=color_type)
-parser.add_argument('color_green', type=color_type)
-parser.add_argument('color_blue', type=color_type)
+parser.add_argument('red', type=color_type)
+parser.add_argument('green', type=color_type)
+parser.add_argument('blue', type=color_type)
 parser.add_argument('rumble', type=rumble_type)
 
 
 def get_controller_by_id(controller_id):
+    if CONTROLLER_SERIALS:
+        serial = CONTROLLER_SERIALS.get(controller_id)
+
+        if not serial:
+            return None
+
+        for controller in controllers:
+            if controller.controller.get_serial() == serial:
+                return controller
+
+        return None
+        
     try:
         return controllers[controller_id]
     except IndexError:
-        abort(404, message="Controller {} doesn't exist".format(controller_id))
+        return None
 
 
 class ControllerListResource(Resource):
@@ -50,16 +85,24 @@ class ControllerResource(Resource):
     def get(self, controller_id):
         controller = get_controller_by_id(controller_id)
 
+        if controller is None:
+            abort(404, message="Controller {} doesn't exist".format(controller_id))
+
+        controller.last_accessed = datetime.datetime.now()
+
         return controller.state_as_dict()
 
     def put(self, controller_id):
         controller = get_controller_by_id(controller_id)
+        
+        if controller is None:
+            abort(404, message="Controller {} doesn't exist".format(controller_id))
 
         args = parser.parse_args()
         
-        red = args['color_red']
-        green = args['color_green']
-        blue = args['color_blue']
+        red = args['red']
+        green = args['green']
+        blue = args['blue']
         
         controller.set_color(red, green, blue)
 
@@ -71,14 +114,44 @@ class ControllerResource(Resource):
         return 'Updated', 201
 
 
-api.add_resource(ControllerListResource, '/controllers')
-api.add_resource(ControllerResource, '/controllers/<int:controller_id>')
+api.add_resource(ControllerListResource, '/controllers/')
+api.add_resource(ControllerResource, '/controllers/<int:controller_id>/')
 
 
-if __name__ == '__main__':
-    controllers = get_controllers()
+def battery_saver_factory(seconds):
+    def battery_saver():
+        while(True):
+            for controller in controllers:
+                if controller.red or controller.green or controller.blue or controller.rumble:
+                    time_since_last_accessed = datetime.datetime.now() - controller.last_accessed
 
-    app.run(debug=True, use_reloader=False)
+                    if (time_since_last_accessed.days * 86400 + time_since_last_accessed.seconds > seconds):
+                        controller.set_color(0, 0, 0)
+                        controller.set_rumble(0)
+
+            time.sleep(0.5)
+
+    return battery_saver
+
+
+def main():
+    arguments = docopt(__doc__, version='PS Move Restful Server')
+    battery_saver_mode = arguments['--battery-saver']
+
+    for controller in controllers:
+        controller.last_accessed = datetime.datetime.now()
+
+    if battery_saver_mode:
+        battery_saver_thread = Thread(target=battery_saver_factory(3))
+        battery_saver_thread.daemon = True
+        battery_saver_thread.start()
+
+    app.run(debug=False, use_reloader=False)
 
     for controller in controllers:
         controller.terminate()
+
+
+if __name__ == '__main__':
+    main()
+
--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import sys, os, time
 from threading import Thread
+import requests
 
-sys.path.insert(0, os.environ['PSMOVEAPI_BUILD_DIR'])
-import psmove
+PSMOVEAPI_BUILD_DIR = os.environ.get('PSMOVEAPI_BUILD_DIR')
+
+if PSMOVEAPI_BUILD_DIR:
+    sys.path.insert(0, os.environ['PSMOVEAPI_BUILD_DIR'])
+    import psmove
 
 
 class Controller(object):
@@ -12,9 +16,9 @@ class Controller(object):
     controller = None
     read_only = False
 
-    color_red = 0
-    color_green = 0
-    color_blue = 0
+    red = 0
+    green = 0
+    blue = 0
 
     ax = 0
     ay = 0
@@ -32,17 +36,14 @@ class Controller(object):
     btn_t = False
     btn_ps = False
     battery = 0
+    trigger = 0
 
     rumble = 0
 
     def __init__(self, controller, read_only=False):
-        self._active = True
         self.controller = controller
         self.read_only = read_only
-
-        self._loop_thread = Thread(target=self._loop)
-        self._loop_thread.daemon = True
-        self._loop_thread.start()
+        self.start_loop()
 
     def terminate(self):
         self._active = False
@@ -53,11 +54,18 @@ class Controller(object):
     def _loop(self):
         while(self._active):
             if not self.read_only:
-                self.controller.set_leds(self.color_red, self.color_green, self.color_blue)
+                self.controller.set_leds(self.red, self.green, self.blue)
                 self.controller.update_leds()
+                self.controller.set_rumble(self.rumble)
 
             self.update_state()
-            time.sleep(0.012)
+            time.sleep(0.01)
+
+    def start_loop(self):
+        self._active = True
+        self._loop_thread = Thread(target=self._loop)
+        self._loop_thread.daemon = True
+        self._loop_thread.start()
 
     def update_state(self):
         result = self.controller.poll()
@@ -76,6 +84,7 @@ class Controller(object):
             self.btn_t = bool(buttons & psmove.Btn_T)
             self.btn_ps = bool(buttons & psmove.Btn_PS)
             self.battery = self.controller.get_battery()
+            self.trigger = self.controller.get_trigger()
 
             self.ax, self.ay, self.az = self.controller.get_accelerometer_frame(psmove.Frame_SecondHalf)
             self.gx, self.gy, self.gz = self.controller.get_gyroscope_frame(psmove.Frame_SecondHalf)
@@ -97,40 +106,186 @@ class Controller(object):
             'btn_move': self.btn_move,
             'btn_t': self.btn_t,
             'btn_ps': self.btn_ps,
-            'color_red': self.color_red,
-            'color_green': self.color_green,
-            'color_blue': self.color_blue,
+            'battery': self.battery,
+            'trigger': self.trigger,
+            'red': self.red,
+            'green': self.green,
+            'blue': self.blue,
             'rumble': self.rumble,
         }
 
         # There's currently no way to get color
         # or rumble directly from the controller
         if self.read_only:
-            del state_dict['color_red']
-            del state_dict['color_green']
-            del state_dict['color_blue']
+            del state_dict['red']
+            del state_dict['green']
+            del state_dict['blue']
             del state_dict['rumble']
 
         return state_dict
 
     def set_color(self, red=None, green=None, blue=None):
         if red is not None:
-            self.color_red = red
+            self.red = red
 
         if green is not None:
-            self.color_green = green
+            self.green = green
 
         if blue is not None:
-            self.color_blue = blue
-
-        self.controller.set_leds(self.color_red, self.color_green, self.color_blue)
-        self.controller.update_leds()
+            self.blue = blue
 
     def set_rumble(self, rumble):
         self.rumble = rumble
-        self.controller.set_rumble(rumble)
+
+    def on_btn_triangle(self, fn, *args, **kwargs):
+        callback = Callback(fn, *args, **kwargs)
+        _btn_triangle_callbacks.append(callback)
+        return callback
+
+
+class RemoteController(Controller):
+    _red = 0
+    _green = 0
+    _blue = 0
+    _rumble = 0
+    _dirty = True  # Default to True so values get cleared on startup
+
+    def __init__(self, url):
+        self.url = url
+        self.start_loop()
+    
+    def _loop(self):
+        while(self._active):
+            self.update_state()
+            time.sleep(0.02)
+
+            if self._dirty:
+                self.update_remote_state()
+                self._dirty = False
+
+    def terminate(self):
+        # Let the loop do its thing until
+        # we're not dirty any more
+        while(self._dirty):
+            time.sleep(0.02)
+
+        self._active = False
+
+        if self._loop_thread:
+            self._loop_thread.join()
+
+    def update_remote_state(self):
+        data = {
+            'red': self.red,
+            'green': self.green,
+            'blue': self.blue,
+            'rumble': self.rumble,
+        }
+
+        try:
+            response = requests.put(self.url, data)
+        except requests.ConnectionError:
+            print "Could not connect to controller at %s" % self.url
+            self._active = False
+            return
+
+        if response.status_code == 404:
+            print "Controller not found at %s" % self.url
+            self._active = False
+            return
+        elif not response.ok:
+            print "Encountered error updating controller: %s (%s)" % (response.status_code, response.reason)
+            self._active = False
+            return
+
+    @property
+    def red(self):
+        return self._red
+
+    @red.setter
+    def red(self, val):
+        self._red = val
+        self._dirty = True
+
+    @property
+    def green(self):
+        return self._green
+
+    @green.setter
+    def green(self, val):
+        self._green = val
+        self._dirty = True
+
+    @property
+    def blue(self):
+        return self._blue
+
+    @blue.setter
+    def blue(self, val):
+        self._blue = val
+        self._dirty = True
+
+    @property
+    def rumble(self):
+        return self._rumble
+
+    @rumble.setter
+    def rumble(self, val):
+        self._rumble = val
+        self._dirty = True
+
+    def update_state(self):
+        try:
+            response = requests.get(self.url)
+        except requests.ConnectionError:
+            print "Could not connect to controller at %s" % self.url
+            self._active = False
+            return
+
+        if response.status_code == 404:
+            print "Controller not found at %s" % self.url
+            self._active = False
+            return
+        elif not response.ok:
+            print "Encountered error updating controller: %s (%s)" % (response.status_code, response.reason)
+            self._active = False
+            return
+
+        result = response.json()
+
+        self.btn_triangle = result.get('btn_triangle')
+        self.btn_circle = result.get('btn_circle')
+        self.btn_cross = result.get('btn_cross')
+        self.btn_square = result.get('btn_square')
+        self.btn_select = result.get('btn_select')
+        self.btn_start = result.get('btn_start')
+        self.btn_move = result.get('btn_move')
+        self.btn_t = result.get('btn_t')
+        self.btn_ps = result.get('btn_ps')
+        self.battery = result.get('battery')
+        self.ax = result.get('ax')
+        self.ay = result.get('ay')
+        self.az = result.get('ax')
+        self.gx =  result.get('gx')
+        self.gy = result.get('gy')
+        self.gz = result.get('gz')
+
+
+class Callback(object):
+    def __init__(self, fn, *args, **kwargs):
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.fn(*self.args, **self.kwargs)
+
 
 
 def get_controllers(read_only=False):
     controllers = [psmove.PSMove(x) for x in range(psmove.count_connected())]
     return [Controller(c, read_only) for c in controllers if c.connection_type == psmove.Conn_Bluetooth]
+
+
+def get_remote_controller(url):
+    return RemoteController(url)
